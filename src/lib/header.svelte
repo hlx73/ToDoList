dd11a806e580565000c615a87ee5ed9af2ef1a1b
Hadeel <hadelturki@hotmail.com>
2024-06-27 16:48:12 +0300

    header

---
+++ src/lib/header.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import dayjs from "dayjs";
+  //import 'dayjs/locale/ar'اذا ابغا الكتابة عربي
+  //dayjs.locale('de')  use locale globally
+  let period = dayjs().format("a") == "pm" ? "evening" : "morning";
+</script>
+
+<header class="flex justify-between">
+  <div>
+    <h1 class="text-2xl mb-2">
+      Good {period} 👋
+    </h1>
+    <h2 class="text-[#707178] text-">
+      {dayjs().format("dddd D MMM YYYY")}
+    </h2>
+  </div>
+  <div class="bg-red-500 h-16 w-32"></div>
+</header>
