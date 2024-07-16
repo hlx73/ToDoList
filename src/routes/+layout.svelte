85e8c8eafb699292a4afa2c6a445943172ddbb2f
Hadeel <hadelturki@hotmail.com>
2024-07-16 18:39:46 +0300

    last

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,20 @@
 <script lang="ts">
 	import '../app.postcss';
 	import { initializeStores, Modal } from '@skeletonlabs/skeleton';
+	//@ts-ignore
+	import { pwaInfo } from 'virtual:pwa-info'; 
 
 initializeStores();
+$: webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : '' 
+
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 </script>
+<svelte:head> 
+ 	{@html webManifestLink} 
+</svelte:head>
 <Modal buttonPositive="variant-filled-error"/>
 
 <!-- <AppShell>...</AppShell> -->
